a13fa7ffd5c1e9eb7ddc11c2ec0ba049fc756265
fuchaanyue <138356138+fuchaanyue@users.noreply.github.com>
2025-10-01 14:17:10 +0800

    项目名改成小写

---
+++ wannalearnmoreaboutyou/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for wannalearnmoreaboutyou project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.wsgi import get_wsgi_application
+
+# 添加项目根目录到Python路径
+current_path = Path(__file__).parent
+if str(current_path) not in sys.path:
+    sys.path.append(str(current_path))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wannalearnmoreaboutyou.settings')
+
+application = get_wsgi_application()
